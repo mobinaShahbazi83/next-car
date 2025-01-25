475c4c76bc11acfb5b47b101cf12067ab88fab9b
mobinaShahbazi83 <shzmobina249@gmail.com>
2025-01-25 21:16:31 +0330

    Fix some bugs

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,5 +1,5 @@
 import CarsPage from "@/components/templates/CarsPage"
-import carsData from "../../data/carsdata"
+import carsData from "../../data/carsData"
 import Categories from "@/components/module/Categories"
 import SearchBar from "@/components/module/SearchBar"
 import AllButon from "@/components/module/AllButon"
